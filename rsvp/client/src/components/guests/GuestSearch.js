86fc507bbbaf79fe5e1dbd6448f81b549bc2f13e
Timothy Sharkey <Techne3@gmail.com>
2020-01-11 14:17:43 -0700

    Created A Filter and Search component

---
+++ rsvp/client/src/components/guests/GuestSearch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function GuestSearch() {
+    return (
+        <div>
+        <input type="text" className="search" placeholder=" Search Guest" />
+        <i className="fas fa-search search-icon" />
+      </div>
+    )
+}
+
+export default GuestSearch
--- rsvp/client/src/components/pages/Home.js
+++ rsvp/client/src/components/pages/Home.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import GuestForm from '../../components/guests/GuestForm'
+import React from "react";
+import GuestForm from "../../components/guests/GuestForm";
+import GuestCounter from "../guests/GuestCounter";
+import GuestFilter from "../guests/GuestFilter";
+import GuestSearch from "../guests/GuestSearch";
+import Guests from "../guests/Guests";
 
 function Home() {
-    return (
-        <div className="app-container">
-            <GuestForm />
+  return (
+    <div className="app-container">
+      <div className="main">
+        <div className="filter">
+        <GuestFilter />
+        <GuestSearch />
         </div>
-    )
+        <GuestForm />
+        <GuestCounter />
+      </div>
+      <Guests />
+    </div>
+  );
 }
-export default Home
+export default Home;
