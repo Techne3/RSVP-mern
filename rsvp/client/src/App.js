ecc7f3a31b5d9ef44d11e4f484977295b8b4dd36
Timothy Sharkey <Techne3@gmail.com>
2020-01-11 14:01:52 -0700

    Started client side UI Added Navbar  and Form components Added styling in App.css

---
+++ rsvp/client/src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar'
+import Home from './components/pages/Home'
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ rsvp/client/src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import GuestForm from '../../components/guests/GuestForm'
+
+function Home() {
+    return (
+        <div className="app-container">
+            <GuestForm />
+        </div>
+    )
+}
+export default Home
